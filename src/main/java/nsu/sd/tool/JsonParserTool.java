79629ec4730ea3b944de7e35e0fd7ebb39245712
Odnostorontseva Evgenia <jennyodnost@mail.ru>
2026-03-18 12:10:02 +0700

    сделала, чтобы после фильтрации создавался новый отфильтрованный джсон

--- src/main/java/nsu/sd/tool/JsonParserTool.java
+++ src/main/java/nsu/sd/tool/JsonParserTool.java
@@ -1,64 +1,85 @@
 package nsu.sd.tool;
 
 import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.util.TokenBuffer;
 import nsu.sd.tool.filter.Expression;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.*;
 
 public class JsonParserTool {
 
-    public static List<Map<String,Object>> parse(InputStream input, Expression filter) {
+    public static void parse(InputStream input, OutputStream output, Expression filter) {
 
-        List<Map<String,Object>> result = new ArrayList<>();
         Set<String> neededFields = filter.requiredFields();
 
         try {
-
             JsonFactory factory = new JsonFactory();
+
             JsonParser parser = factory.createParser(input);
+            JsonGenerator generator = factory.createGenerator(output);
+
+            generator.writeStartArray();
 
             Map<String,Object> currentObject = null;
             String currentField = null;
 
             while(parser.nextToken() != null) {
 
-                JsonToken token = parser.currentToken();
-
-                if(token == JsonToken.START_OBJECT) {
+                if(parser.currentToken() == JsonToken.START_OBJECT) {
                     currentObject = new HashMap<>();
-                }
 
-                if(token == JsonToken.FIELD_NAME) {
-                    currentField = parser.currentName();
-                }
+                    TokenBuffer tokenBuffer = new TokenBuffer(parser);
+                    tokenBuffer.copyCurrentStructure(parser);
 
-                if(token.isScalarValue()) {
+                    JsonParser p = tokenBuffer.asParser();
 
-                    if(currentObject != null && neededFields.contains(currentField)) {
-                        currentObject.put(currentField, parser.getValueAsString());
+                    while (p.nextToken() != JsonToken.END_OBJECT) {
+                        JsonToken token = p.currentToken();
+
+                        if(token == JsonToken.FIELD_NAME) {
+                            currentField = p.currentName();
+                        }
+
+                        if(token.isScalarValue()) {
+
+                            if(neededFields.contains(currentField)) {
+                                Object value;
+
+                                JsonToken valueToken = p.currentToken();
+
+                                switch (valueToken) {
+                                    case VALUE_NUMBER_INT -> value = p.getIntValue();
+                                    case VALUE_NUMBER_FLOAT -> value = p.getDoubleValue();
+                                    case VALUE_TRUE, VALUE_FALSE -> value = p.getBooleanValue();
+                                    default -> value = p.getText();
+                                }
+
+                                currentObject.put(currentField, value);
+                            }
+                        }
                     }
 
-                }
+                    if(p.currentToken() == JsonToken.END_OBJECT) {
 
-                if(token == JsonToken.END_OBJECT) {
+                        JsonKeysReader reader = new JsonKeysReader(currentObject);
 
-                    JsonKeysReader reader = new JsonKeysReader(currentObject);
-
-                    if(filter.evaluate(reader)) {
-                        result.add(currentObject);
+                        if(filter.evaluate(reader)) {
+                            tokenBuffer.serialize(generator);
+                        }
+                        currentObject = null;
                     }
-
-                    currentObject = null;
                 }
             }
+            generator.writeEndArray();
+            generator.close();
 
         } catch(Exception e) {
             throw new RuntimeException(e);
         }
-
-        return result;
     }
 }
