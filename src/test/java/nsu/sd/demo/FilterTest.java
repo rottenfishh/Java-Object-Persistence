61d70547d6d67760a7e70e66229c0fef80d4c505
Odnostorontseva Evgenia <jennyodnost@mail.ru>
2026-04-01 10:26:18 +0700

    added nested test

--- src/test/java/nsu/sd/demo/FilterTest.java
+++ src/test/java/nsu/sd/demo/FilterTest.java
@@ -7,9 +7,8 @@ import nsu.sd.tool.filter.Expression;
 import nsu.sd.tool.filter.GreaterThan;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -28,5 +27,47 @@ public class FilterTest {
         Expression filter = new AND(new Equals("name", "Alice"), new GreaterThan("age", 20.0));
 
         assertTrue(filter.requiredFields().contains("name"));
+    }
+
+    @Test
+    void nestedTest() throws IOException {
+        String json = """
+                [
+                  {"name":"Alice",
+                   "info": {
+                    "age":20,
+                    "city":"Moscow"
+                   }
+                  }, 
+                  {"name":"Bob",
+                   "info": {
+                    "age":15,
+                    "city":"Sain P"
+                   }
+                  },
+                  {"name":"Peter",
+                   "info": {
+                    "age":18,
+                    "city":"Moscow"
+                   }
+                  }
+                ]
+               \s""";
+
+        Expression filter = new AND(new Equals("city", "Moscow"), new GreaterThan("age", 17.0));
+
+        byte[] bytes = json.getBytes(StandardCharsets.UTF_8);
+        InputStream in= new ByteArrayInputStream(bytes);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        JsonParserTool.parse(in, out, filter);
+
+        String result = out.toString(StandardCharsets.UTF_8);
+
+        System.out.println(result);
+
+        assertTrue(result.contains("Alice"));
+        assertTrue(result.contains("Peter"));
     }
 }
